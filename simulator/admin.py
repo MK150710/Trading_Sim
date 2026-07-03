6093553cad6caf429541055750d47158d8b7e520
MK150710 <maanvikkharbanda2@gmail.com>
2026-07-03 14:33:29 +0530

    Feat: Created Admin panel and superuser

--- simulator/admin.py
+++ simulator/admin.py
@@ -1,3 +1,121 @@
 from django.contrib import admin
+from .models import Portfolio, Stock, Holding, Transaction, Wishlist
 
-# Register your models here.
+@admin.register(Portfolio)
+class PortfolioAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "user", 
+        "current_balance", 
+        "created_at", 
+        "updated_at"
+    )
+
+    search_fields = (
+        "user__username", 
+        "user__email"
+    )
+
+    ordering = ("user__username",)
+
+
+@admin.register(Stock)
+class StockAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "symbol",
+        "company_name",
+        "exchange", 
+        "current_price",
+        "previous_close",
+        "is_featured",
+        "last_updated"
+    )
+
+    search_fields = (
+        "symbol",
+        "company_name"
+    )
+
+    list_filter = (
+        "exchange", 
+        "is_featured"
+    )
+
+    list_editable = ("is_featured",)
+
+    ordering = ("symbol",)
+
+@admin.register(Holding)
+class HoldingAdmin(admin.ModelAdmin):
+    
+    list_display = (
+        "portfolio",
+        "stock",
+        "quantity",
+        "avg_buy_price",
+        "total_investment"
+    )
+
+    search_fields = (
+        "portfolio__user__username",
+        "stock__symbol",
+        "stock__company_name",
+    )
+
+    list_filter = ("stock__exchange",)
+
+    ordering = (
+        "portfolio__user__username",
+        "stock__symbol"
+    )
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "portfolio",
+        "stock",
+        "transaction_type",
+        "shares_traded",
+        "price_on_trade",
+        "balance_before",
+        "balance_after",
+        "traded_at"
+    )
+
+    search_fields = (
+        "portfolio__user__username",
+        "stock__symbol",
+        "stock__company_name"
+    )
+
+    list_filter = (
+        "transaction_type",
+        "stock__exchange"
+    )
+
+    ordering = ("-traded_at",)
+
+@admin.register(Wishlist)
+class WishlistAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "user",
+        "stock",
+        "added_at"
+    )
+
+    search_fields = (
+        "user__username",
+        "user__email",
+        "stock__symbol",
+        "stock__company_name"
+    )
+
+    list_filter = ("stock__exchange",)
+
+    ordering = (
+        "user__username",
+        "stock__symbol"
+    )
--- simulator/urls.py
+++ simulator/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.contrib import admin
 from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
     path("register", views.register, name="register"),
     path("login", views.login, name="login"),
+    path('admin/', admin.site.urls),
 ]
