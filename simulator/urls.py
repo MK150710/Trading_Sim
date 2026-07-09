e26c335eabb0f53b5d55f4866235542aaf60d756
MK150710 <maanvikkharbanda2@gmail.com>
2026-07-09 23:33:21 +0530

    Feat: Added all paths for API endpoints

--- simulator/urls.py
+++ simulator/urls.py
@@ -8,5 +8,9 @@ urlpatterns = [
     path("login", views.login, name="login"),
     path("admin/", admin.site.urls),
     path("logout", views.logout, name="logout"),
-    path("dashboard", views.dashboard, name="dashboard")
+    path("dashboard", views.dashboard, name="dashboard"),
+    path("api/portfolio", views.dashboard_api, name="portfolio_api"),
+    path("api/watchlist", views.get_watchlist, name="watchlist_api"),
+    path("api/transactions", views.get_transactions, name="transaction_api"),
+    path("api/portfolio/history", views.get_portfolio_history, name="portfolio_history")
 ]
