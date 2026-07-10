a9dfc3e7eab7b289ce841fcc5b6f0f2b75809bfc
MK150710 <maanvikkharbanda2@gmail.com>
2026-07-10 14:08:00 +0530

    Feat: Introduced cache to temporarily store market data

--- trading/settings.py
+++ trading/settings.py
@@ -123,3 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+        "LOCATION": "tradesims-cache",
+    }
+}
