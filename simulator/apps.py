865be0d99d81ebaac000a1522a5a1c38995f2608
MK150710 <maanvikkharbanda2@gmail.com>
2026-07-12 22:38:04 +0530

    Feat: Included apschedular in apps to start on runserver

--- simulator/apps.py
+++ simulator/apps.py
@@ -1,5 +1,17 @@
 from django.apps import AppConfig
+import os
 
 
 class SimulatorConfig(AppConfig):
     name = 'simulator'
+    default_auto_field = "django.db.models.BigAutoField"
+
+    def ready(self):
+
+        if os.environ.get("RUN_MAIN") != "true":
+            return
+
+        from .scheduler import scheduler
+
+        if not scheduler.running:
+            scheduler.start()
