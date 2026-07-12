7ec777a3726d2c1d3fda374ad87667fe429eca9b
MK150710 <maanvikkharbanda2@gmail.com>
2026-07-12 22:36:41 +0530

    Created a list of 112 stocks that should be regularly updated

---
+++ simulator/static/simulator/top_stocks.py
@@ -0,0 +1,46 @@
+TOP_STOCKS = [
+    # Magnificent 7
+    "AAPL", "MSFT", "NVDA", "AMZN", "GOOGL", "META", "TSLA",
+
+    # Semiconductors
+    "AMD", "AVGO", "TSM", "QCOM", "MU", "INTC", "ARM",
+    "ASML", "AMAT", "LRCX", "KLAC", "ADI", "TXN", "ON",
+
+    # AI / Software / Cloud
+    "PLTR", "CRM", "ORCL", "ADBE", "NOW", "SNOW", "CRWD",
+    "PANW", "NET", "DDOG", "MDB", "SHOP", "UBER", "SAP",
+
+    # Finance
+    "JPM", "BAC", "WFC", "GS", "MS", "BLK",
+    "V", "MA", "AXP", "SCHW", "PYPL",
+
+    # Healthcare
+    "LLY", "JNJ", "UNH", "ABBV", "MRK",
+    "PFE", "ABT", "TMO", "ISRG", "VRTX",
+
+    # Consumer
+    "COST", "WMT", "HD", "LOW", "MCD",
+    "NKE", "SBUX", "DIS", "NFLX", "KO",
+    "PEP", "PG", "CMG",
+
+    # Industrials
+    "CAT", "GE", "RTX", "LMT", "HON",
+    "BA", "DE", "UPS", "UNP",
+
+    # Energy
+    "XOM", "CVX", "COP", "SLB", "EOG",
+
+    # Telecom / Media
+    "TMUS", "VZ", "T", "CMCSA", "CHTR",
+
+    # EV / Auto
+    "F", "GM", "RIVN", "LCID",
+
+    # Retail favorites / High-volume
+    "GME", "AMC", "COIN", "HOOD", "MSTR",
+    "SMCI", "RKLB", "SOFI", "CAVA", "HIMS",
+
+    # ETFs
+    "SPY", "QQQ", "DIA", "VOO", "VTI",
+    "IWM", "ARKK", "XLK", "XLF", "XLE"
+]
