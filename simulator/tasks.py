12662b34e692372d1e4805a0745d019544681520
Maanvik <maanvikkharbanda2@gmail.com>
2026-08-05 22:20:40 +0530

    BugFix: Checked for NaN values

--- simulator/tasks.py
+++ simulator/tasks.py
@@ -1,6 +1,7 @@
 from .static.simulator.top_stocks import TOP_STOCKS
 from .services.get_quote_data import get_data
 from .models import Stock
+from math import isnan
 
 def update_stocks():
 
@@ -16,5 +17,7 @@ def update_stocks():
         )
 
         for field, value in stock_data.items():
+            if isinstance(value, float) and isnan(value):
+                print(f"{symbol}: {field} = NaN")
             setattr(stock, field, value)
         stock.save()
