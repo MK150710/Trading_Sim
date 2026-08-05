55a8189c0ffd17d8bd9b272f9af2c6ba98b4a5a7
Maanvik <maanvikkharbanda2@gmail.com>
2026-08-05 22:23:48 +0530

    Feat: Created file to get statistics

--- simulator/services/Stock_Page/statistics.py
+++ simulator/services/Stock_Page/statistics.py
@@ -1,25 +1,45 @@
+import pandas as pd
 import yfinance as yf
 
+
+def clean(value):
+    return None if pd.isna(value) else value
+
+
 def get_stats(symbol):
+    try:
+        ticker = yf.Ticker(symbol)
+        info = ticker.info
 
-    ticker = yf.Ticker(symbol)
-    info = ticker.info
+        return {
+            "open": clean(info.get("open")),
+            "prevClose": clean(info.get("previousClose")),
+            "dayHigh": clean(info.get("dayHigh")),
+            "dayLow": clean(info.get("dayLow")),
+            "week52High": clean(info.get("fiftyTwoWeekHigh")),
+            "week52Low": clean(info.get("fiftyTwoWeekLow")),
+            "marketCap": clean(info.get("marketCap")),
+            "volume": clean(info.get("volume")),
+            "avgVolume": clean(info.get("averageVolume")),
+            "peRatio": clean(info.get("trailingPE")),
+            "eps": clean(info.get("trailingEps")),
+            "dividendYield": clean(info.get("dividendYield")),
+            "beta": clean(info.get("beta")),
+        }
 
-    stats = {
-        "open": info.get("open"),
-        "prevClose": info.get("previousClose"),
-        "dayHigh": info.get("dayHigh"),
-        "dayLow": info.get("dayLow"),
-        "week52High": info.get("fiftyTwoWeekHigh"),
-        "week52Low": info.get("fiftyTwoWeekLow"),
-        "marketCap": info.get("marketCap"),
-        "volume": info.get("volume"),
-        "avgVolume": info.get("averageVolume"),
-        "peRatio": info.get("trailingPE"),
-        "eps": info.get("trailingEps"),
-        "dividendYield": info.get("dividendYield"),
-        "beta": info.get("beta"),
-    }
-
-    return stats
-
+    except Exception:
+        return {
+            "open": None,
+            "prevClose": None,
+            "dayHigh": None,
+            "dayLow": None,
+            "week52High": None,
+            "week52Low": None,
+            "marketCap": None,
+            "volume": None,
+            "avgVolume": None,
+            "peRatio": None,
+            "eps": None,
+            "dividendYield": None,
+            "beta": None,
+        }
