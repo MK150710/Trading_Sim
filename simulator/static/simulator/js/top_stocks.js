db6dfcac97b7075b8d65bb7064c59b53ea1a0efc
Maanvik <maanvikkharbanda2@gmail.com>
2026-08-06 13:54:11 +0530

    Chore: Created a new file which stores alll stock symbols which are in database

---
+++ simulator/static/simulator/js/top_stocks.js
@@ -0,0 +1,41 @@
+window.TOP_STOCKS = [
+    
+    "AAPL", "MSFT", "NVDA", "AMZN", "GOOGL", "META", "TSLA",
+
+
+    "AMD", "AVGO", "TSM", "QCOM", "MU", "INTC", "ARM",
+    "ASML", "AMAT", "LRCX", "KLAC", "ADI", "TXN", "ON",
+
+
+    "PLTR", "CRM", "ORCL", "ADBE", "NOW", "SNOW", "CRWD",
+    "PANW", "NET", "DDOG", "MDB", "SHOP", "UBER", "SAP",
+
+
+    "JPM", "BAC", "WFC", "GS", "MS", "BLK",
+    "V", "MA", "AXP", "SCHW", "PYPL",
+
+
+    "LLY", "JNJ", "UNH", "ABBV", "MRK",
+    "PFE", "ABT", "TMO", "ISRG", "VRTX",
+
+
+    "COST", "WMT", "HD", "LOW", "MCD",
+    "NKE", "SBUX", "DIS", "NFLX", "KO",
+    "PEP", "PG", "CMG", "BRK-B",
+
+    "CAT", "GE", "RTX", "LMT", "HON",
+    "BA", "DE", "UPS", "UNP",
+
+    "XOM", "CVX", "COP", "SLB", "EOG",
+
+    "TMUS", "VZ", "T", "CMCSA", "CHTR",
+
+    "F", "GM", "RIVN", "LCID",
+
+    "GME", "AMC", "COIN", "HOOD", "MSTR",
+    "SMCI", "RKLB", "SOFI", "CAVA", "HIMS",
+
+
+    "SPY", "QQQ", "DIA", "VOO", "VTI",
+    "IWM", "ARKK", "XLK", "XLF", "XLE"
+]
